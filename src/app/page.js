66265d1c57ccd16c9b4d1c8618a3363caf719cff
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2024-08-16 22:32:44 +0530

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Hero from "@/components/Hero/Hero";
+import NavBar from "@/components/NavBar/NavBar";
+
+export default function Home() {
+  return (
+		<div>
+			<NavBar />
+			<main>
+				<Hero />
+			</main>
+		</div>
+	);
+}
