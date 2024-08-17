2b3d3ac5ed3a10cd13a9c3e14cbf3c8865d94588
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2024-08-17 20:42:53 +0530

    feature section

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
 import Footer from "@/components/Footer/Footer";
 import Hero from "@/components/Hero/Hero";
 import NavBar from "@/components/NavBar/NavBar";
+import Features from "@/components/Features/Features";
 
 export default function Home() {
   return (
 		<div>
 			<NavBar />
 			<main>
-				<Hero />
+        <Hero />
+        <Features />
       </main>
       <Footer/>
 		</div>
--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import './features.css'
 
 const Features = () => {
   return (
-    <div>Features</div>
-  )
+		<div className="contain feature">
+			<div className="feature-item">
+				<div className="feature-logo">
+					<img src="/icons/pencil-solid.svg" alt="" />
+				</div>
+				<h3 className="feature-title">AI-Powered Writing</h3>
+				<p className="feature-content">
+					Generate high-quality content with advanced AI technology.
+				</p>
+			</div>
+			<div className="feature-item">
+				<div className="feature-logo">
+					<img src="/icons/book-solid.svg" alt="" />
+				</div>
+				<h3 className="feature-title">Custom Genere</h3>
+				<p className="feature-content">
+					Create stories in any genre or blend multiple styles.
+				</p>
+			</div>
+			<div className="feature-item">
+				<div className="feature-logo">
+					<img src="/icons/download-solid.svg" alt="" />
+				</div>
+				<h3 className="feature-title">Instant Download</h3>
+				<p className="feature-content">
+					Get your completed book in various formats instantly.
+				</p>
+			</div>
+			<div className="feature-item">
+				<div className="feature-logo">
+					<img src="/icons/language-solid.svg" alt="" />
+				</div>
+				<h3 className="feature-title">Multilingual</h3>
+				<p className="feature-content">
+					Generate content in multiple languages effortlessly.
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export default Features
