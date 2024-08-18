b27e43d08760e83f9b5139c2791d04f1762f505e
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2024-08-18 18:04:30 +0530

    stay updated section

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Features from "@/components/Features/Features";
 import Working from "@/components/Work/Working";
 import Roadmap from "@/components/Work/Roadmap"; 
 import Samples from "@/components/Samples/Samples";
+import StayUpdated from "@/components/StayUpdated/StayUpdated";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
         <Working />
         <Samples />
         <Roadmap />
+        <StayUpdated />
       </main>
       <Footer/>
 		</div>
---
+++ src/components/StayUpdated/StayUpdated.jsx
@@ -0,0 +1,51 @@
+'use client'
+import React, { useState } from "react";
+import "./stayupdated.css";
+
+const StayUpdated = () => {
+	const [email, setEmail] = useState("");
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		if (!email) {
+			alert("Please enter an email address.");
+			return;
+		}
+
+		try {
+			// api calls to store email into database or verifying email.
+		} catch (error) {
+			console.error("Error submitting email:", error);
+			alert("An error occurred. Please try again later.");
+		}
+	};
+
+	return (
+		<section className="contain">
+			<div className="stay-updated">
+				<h2 className="updated-title">Stay Updated</h2>
+				<p className="updated-content">
+					Subscribe to our newsletter for the latest AI writing tips and BookAI
+					updates.
+				</p>
+
+				<form onSubmit={handleSubmit}>
+					<input
+						type="text"
+						placeholder="Email"
+            value={email}
+            className="updated-input"
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+
+					<button type="submit" className="updated-submit" >
+						Subscribe
+					</button>
+				</form>
+			</div>
+		</section>
+	);
+};
+
+export default StayUpdated;
